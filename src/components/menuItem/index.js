0fa95a56969ff850ca7555e814a028969ecd143b
Robson Aires <robsonaires@outlook.com.br>
2023-05-16 22:44:34 -0300

    estrutura do site finalido 3/3

--- src/components/menuItem/index.js
+++ src/components/menuItem/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { LinkArea, LinkIcon } from './styled';
+import { useHistory, useLocation } from 'react-router-dom';
 
 export default ({ icon, link }) => {
+    const history = useHistory();
+    const location = useLocation();
+
+    let isActive = location.pathname === link;
+
+    const handleLinkClick = (e) => {
+        e.preventDefault();
+        history.push( link );
+    }
+
     return (
-        <LinkArea href={link}>
+        <LinkArea active={isActive} href={link} onClick={handleLinkClick}>
             <LinkIcon src={icon} />
             </LinkArea>
     )
--- src/components/menuItem/styled.js
+++ src/components/menuItem/styled.js
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 export const LinkArea = styled.a`
 display: flex;
 justify-content: center;
-alight-items: center;
+align-items: center;
 width: 60px;
 height: 60px;
+background-color: ${props=>props.active ? '#0b4d0b' : 'transparent'};
 border-radius: 10px;
 margin-bottom: 10px;
 `;
