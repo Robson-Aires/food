8e19b5604e90e08d590d22a6b3bd13d0c0fa0629
Robson Aires <robsonaires@outlook.com.br>
2023-05-16 22:26:41 -0300

    estou fazendo a estrutura do site 2/3 concluido

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { useSelector } from 'react-redux';
 
+import { Container, Menu, PageBody } from './AppStyled'
+
 import HomeScreen from './pages/HomeScreen';
 import Tela2Screen from './pages/Tela2Screen';
 
+import MenuItem from './components/menuItem/index'
+
 export default () => {
-    const name = useSelector(state => state.user.name);
+const name = useSelector(state => state.user.name);
 
     return (
         <BrowserRouter>
-            <h1>Oi, {name}</h1>
-
-            <Switch>
-                <Route exact path="/">
-                    <HomeScreen />
-                </Route>
-                <Route path="/tela2/:nome">
-                    <Tela2Screen />
-                </Route>
-            </Switch>
+        <Container>
+            <Menu>
+            <MenuItem icon="/assets/store.png" link="/"/>
+            <MenuItem icon="/assets/order.png" link="/orders"/>
+            <MenuItem icon="/assets/profile.png" link="/profile"/>
+            </Menu>
+            {/* corpo da pagina */}
+            <PageBody>
+                <Switch>
+                    <Route exact path="/">
+                        <HomeScreen />
+                    </Route>
+                    <Route path="/tela2/:nome">
+                        <Tela2Screen />
+                    </Route>
+                </Switch>
+            </PageBody>
+            {/* carrinho de compras
+            <Cart></Cart> */}
+        </Container>
         </BrowserRouter>
     );
 }
