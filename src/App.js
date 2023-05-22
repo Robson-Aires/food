90ae940132bb2b8847ec11ca54b2c1189af72f24
Robson Aires <robsonaires@outlook.com.br>
2023-05-22 18:13:53 -0300

    finalizando o controle de acesso do site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { useSelector } from 'react-redux';
-
 import { Container, Menu, PageBody } from './AppStyled'
-
 import HomeScreen from './pages/HomeScreen';
 import Tela2Screen from './pages/Tela2Screen';
-
-import MenuItem from './components/menuItem/index'
+import MenuItem from './components/menuItem/index';
+import PrivateRoute from './components/PrivateRoute';
 
 export default () => {
 const name = useSelector(state => state.user.name);
@@ -26,6 +24,12 @@ const name = useSelector(state => state.user.name);
                     <Route exact path="/">
                         <HomeScreen />
                     </Route>
+                    <PrivateRoute path="/orders"> 
+                    <div>TELA DE PEDIDOS</div>
+                    </PrivateRoute>
+                    <PrivateRoute path="/profile"> 
+                    <div>TELA DE PERFIL</div>
+                    </PrivateRoute>
                     <Route path="/tela2/:nome">
                         <Tela2Screen />
                     </Route>
