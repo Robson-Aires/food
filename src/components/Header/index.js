b382c1aa43184d84b6f30cc32f88012bb2920319
Robson Aires <robsonaires@outlook.com.br>
2023-05-25 15:03:28 -0300

    fazendo o campo de busca(home) 2/3

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Logo, SearchInput } from './styled'
 
 export default () => {
+    const [inputActive, setInputActive] = useState(false);
+
+    const handleInputFocus = () => {
+        setInputActive(true);
+    }
+
+    const handleInputBlur = () => {
+        setInputActive(false);
+    }
+
     return (
         <Container>
             <Logo src="/assets/logo.png"/>
-            <SearchInput type="text" placeholder='Digite um produto...'/>
+            <SearchInput
+             type="text"
+              placeholder='Digite um produto...'
+              active={inputActive}
+              onFocus={handleInputFocus}
+              onBlur={handleInputBlur}
+              />
         </Container>
     )
 }
