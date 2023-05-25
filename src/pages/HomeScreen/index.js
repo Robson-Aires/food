6125af4caab18e397c9268cbfbd1c2233cb67473
Robson Aires <robsonaires@outlook.com.br>
2023-05-25 14:42:57 -0300

    fazendo o campo de busca(home) 1/3

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { useHistory } from "react-router-dom";
-import { Container, Titulo } from './styled';
+// import { useHistory } from "react-router-dom";
+import { Container } from './styled';
+
+import Header from '../../components/Header/index'
 
 export default () => {
-    const history = useHistory();
+    // const history = useHistory();
 
-    const handleButtonClick = () => {
-        history.push('/tela2/testador');
-    }
 
     return (
         <Container>
-            <Titulo>Homepage</Titulo>
-            <button onClick={handleButtonClick}>Ir para Tela 2</button>
+            <Header />
         </Container>
     );
 }
