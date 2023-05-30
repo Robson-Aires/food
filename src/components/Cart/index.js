8b9ea76dd021c029ce162831953ba929f02f9ccf
Robson Aires <robsonaires@outlook.com.br>
2023-05-29 21:12:09 -0300

    exibição do carrinho finalizado

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,15 +1,39 @@
 import React from "react";
-import { CartArea, CartHeader, CartIcon, CartText, CartBody, } from "./styled";
-import { useSelector } from "react-redux";
+import {
+    CartArea,
+    CartHeader,
+    CartIcon,
+    CartText,
+    CartBody,
+    ProductsArea,
+    ProductItem,
+    ProductPhoto,
+    ProductInfoArea,
+    ProductName,
+    ProductPrice,
+    ProductQuantityArea,
+    ProductQtIcon,
+    ProductQtText,
+     } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 
+
 const Cart = () => {
+    const dispatch = useDispatch();
     const products = useSelector(state => state.cart.products);
     const [show, setShow] = useState(false);
 
     const handleCartClick = () => {
         setShow(!show);
     }
+    const handleProductChange = (key, type) => {
+        dispatch({
+            type: 'CHANGE_PRODUCT',
+            payload:{key, type}
+        });
+    }
+
     return (
         <CartArea>
             <CartHeader onClick={handleCartClick}>
@@ -18,7 +42,28 @@ const Cart = () => {
             {show && <CartIcon src="/assets/down.png" />} 
             </CartHeader>
             <CartBody show={show}>
-                <div style={{width: 50, height: 300, backgroundColor: 'red', }}></div>
+                <ProductsArea>
+                    {products.map((item, index)=> (
+                    <ProductItem key={index}>
+                        <ProductPhoto src={item.image}/>
+                        <ProductInfoArea>
+                            <ProductName>{item.name}</ProductName>
+                            <ProductPrice>R$ {item.price.toFixed(2)}</ProductPrice>
+                        </ProductInfoArea>
+                        <ProductQuantityArea>
+                            <ProductQtIcon
+                             src="/assets/minus.png"
+                             onClick={()=>handleProductChange(index, '-')}
+                              />
+                            <ProductQtText>{item.qt}</ProductQtText>
+                            <ProductQtIcon
+                             src="/assets/plus.png"
+                             onClick={()=>handleProductChange(index, '+')}
+                              />
+                        </ProductQuantityArea>
+                    </ProductItem>
+                    ))}
+                </ProductsArea>
             </CartBody>
         </CartArea>
     )
--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -5,13 +5,13 @@ const initialState = {
     delivery:0,
 };
 
-const reducer = (state = initialState, action) => {
+const Cart = (state = initialState, action) => {
+    let products = [...state.products];
     switch(action.type) {
         case 'ADD_PRODUCT':
-            let products = [...state.products];
             let id = action.payload.data.id;
-
             let index = products.findIndex(item => item.id === id);
+
             if(index > -1) {
                 products[index].qt += action.payload.qt;
             } else {
@@ -20,11 +20,28 @@ const reducer = (state = initialState, action) => {
                     qt: action.payload.qt
                 });
             }
-            console.log(products);
-            return { ...state, products: products };
+
+            return { ...state, products };
+        
+        case 'CHANGE_PRODUCT':
+            switch(action.payload.type) {
+                case '-':
+                    products[action.payload.key].qt--;
+                    if(products[action.payload.key].qt <= 0) {
+                        products = products.filter((item, index) => index !== action.payload.key);
+                    }
+                    break;
+                case '+':
+                    products[action.payload.key].qt++;
+                    break;
+                default:
+                    // Caso padrão para ação desconhecida
+                    break;
+            }
+            return { ...state, products };
+
         default:
-            return state;
+            return state; // Retorno padrão para ação desconhecida
     }
 }
-
-export default reducer;
+export default Cart
